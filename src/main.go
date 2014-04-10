f37b3faeea869bc444f7a8b4eb6b7297fafe21c9
Sebastian Stark <stark@tuebingen.mpg.de>
2014-04-10 23:00:11 +0200

    nicely formatted list output

--- src/main.go
+++ src/main.go
@@ -24,30 +24,33 @@ func main() {
 
     switch cmd {
     case "run": {
-    lastGood := snapshots.lastGood()
-    if lastGood != nil {
-        log.Println("lastgood:", lastGood)
-    } else {
-        log.Println("lastgood: could not find suitable base snapshot")
-    }
-    go CreateSnapshot(c, lastGood)
-    for {
-        select {
-        case e := <-c:
-            if e == nil {
-                log.Println("Snapshot created")
-            } else {
-                log.Println("rsync error:", e)
-            }
-        case <- time.After(time.Hour*10):
-            log.Println("timeout")
-            return
+        lastGood := snapshots.lastGood()
+        if lastGood != nil {
+            log.Println("lastgood:", lastGood)
+        } else {
+            log.Println("lastgood: could not find suitable base snapshot")
         }
-    }
-    }
+        go CreateSnapshot(c, lastGood)
+        for {
+            select {
+            case e := <-c:
+                if e == nil {
+                    log.Println("Snapshot created")
+                } else {
+                    log.Println("rsync error:", e)
+                }
+            case <- time.After(time.Hour*10):
+                log.Println("timeout")
+                return
+            }
+        }
+        }
     case "list": {
         for _, sn := range snapshots {
-            fmt.Println(sn)
+            stime := time.Unix(sn.startTime, 0).Format("2006-01-02 15:04:05")
+            dur := time.Duration(sn.endTime-sn.startTime)*time.Second
+            fmt.Printf("%s (%02dh %02dm %02ds) \"%s\" S%s\n",
+                stime, int(dur.Hours()), int(dur.Minutes()), int(dur.Seconds()), sn.Name(), sn.state)
         }
         os.Exit(0)
     }
