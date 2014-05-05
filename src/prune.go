cab820c15bf180b1b6127b5c9061b45761323c69
Sebastian Stark <stark@tuebingen.mpg.de>
2014-05-05 22:47:04 +0200

    run normal pruning also for highest interval
     - this was a bug: the highest interval was only
       pruned by its special rule. But normal pruning
       should take place there, too.

--- src/prune.go
+++ src/prune.go
@@ -22,24 +22,23 @@ func prune() {
         iv := snapshots.interval(intervals, i).state(STATE_COMPLETE, STATE_OBSOLETE)
         pruneAgain := 0
         if len(iv) > 2 {
-            if i == len(intervals)-2 {
-                // highest interval pruning
-                if (len(iv) > config.MaxKeep) && (config.MaxKeep != 0) {
-                    Debugf("%d snapshots in oldest interval", len(iv))
-                    log.Printf("mark oldest as obsolete: %s", iv[0])
-                    iv[0].transObsolete()
-                    pruneAgain += 1
-                }
-            } else {
-                // last in list is youngest
-                youngest := len(iv) - 1
-                secondYoungest := youngest - 1
-                dist := iv[youngest].startTime.Sub(iv[secondYoungest].startTime)
-                if (dist.Seconds() < intervals[i].Seconds()) {
-                    log.Printf("mark as obsolete: %s", iv[youngest].Name())
-                    iv[youngest].transObsolete()
-                    pruneAgain += 1
-                }
+            // prune highest interval by maximum number
+            if (i == len(intervals)-2) &&
+               (len(iv) > config.MaxKeep) &&
+               (config.MaxKeep != 0) {
+                Debugf("%d snapshots in oldest interval", len(iv))
+                log.Printf("mark oldest as obsolete: %s", iv[0])
+                iv[0].transObsolete()
+                pruneAgain += 1
+            }
+            // regularly prune by sieving
+            youngest := len(iv) - 1
+            secondYoungest := youngest - 1
+            dist := iv[youngest].startTime.Sub(iv[secondYoungest].startTime)
+            if (dist.Seconds() < intervals[i].Seconds()) {
+                log.Printf("mark as obsolete: %s", iv[youngest].Name())
+                iv[youngest].transObsolete()
+                pruneAgain += 1
             }
             if pruneAgain > 0 { prune() }
         }
