987c1df0225ff5bb7e79478b46748dd07d1b2e86
Sebastian Stark <sstark@mailbox.org>
2018-10-12 13:32:28 +0200

    add tests for different rsync exit values

--- rsync_test.go
+++ rsync_test.go
@@ -3,6 +3,8 @@
 package main
 
 import (
+	"os"
+	"path/filepath"
 	"reflect"
 	"testing"
 	"time"
@@ -25,5 +27,41 @@ func TestCreateRsyncCommand(t *testing.T) {
 		"testdata/.data/1400534523-0-incomplete"}
 	if !reflect.DeepEqual(got, wanted) {
 		t.Errorf("wanted %v, got %v", wanted, got)
+	}
+}
+
+func TestFakeRsyncOk(t *testing.T) {
+	var testSnapshots = snapshotList{
+		{time.Unix(1400337531, 0), time.Unix(1400338693, 0), stateComplete},
+		{time.Unix(1400534523, 0), time.Unix(0, 0), stateIncomplete},
+	}
+	var config = config
+	config.repository = "/tmp/snaprd_dest"
+	config.ReadCache()
+	dir, _ := os.Getwd()
+	config.RsyncPath = filepath.Join(dir, "fake_rsync")
+	config.RsyncOpts.Set("--fake_exit=24")
+	_, err := createSnapshot(testSnapshots[0])
+	got := err
+	if got != nil {
+		t.Errorf("createSnapshot() returned an error, but it shouldn't: %v", got)
+	}
+}
+
+func TestFakeRsyncFail(t *testing.T) {
+	var testSnapshots = snapshotList{
+		{time.Unix(1400337531, 0), time.Unix(1400338693, 0), stateComplete},
+		{time.Unix(1400534523, 0), time.Unix(0, 0), stateIncomplete},
+	}
+	var config = config
+	config.repository = "/tmp/snaprd_dest"
+	config.ReadCache()
+	dir, _ := os.Getwd()
+	config.RsyncPath = filepath.Join(dir, "fake_rsync")
+	config.RsyncOpts.Set("--fake_exit=3")
+	_, err := createSnapshot(testSnapshots[0])
+	got := err
+	if got == nil {
+		t.Errorf("createSnapshot() succeded, but it should have failed: %v", got)
 	}
 }
